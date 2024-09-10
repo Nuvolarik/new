c4f00f6acb3d25fb796ef853847f4dc40d1373a1
Paul <paulkuznetsov85@gmail.com>
2024-09-10 20:40:05 +0300

    3rd task

---
+++ test_3.py
@@ -0,0 +1,6 @@
+count_homeworks = 12
+count_hours = 1.5
+nameOfCourse = 'Python'
+timeOfHomework = count_hours/count_homeworks
+
+print('Курс:', nameOfCourse, ',', 'всего задач:', count_homeworks, ',', 'затрачено часов:', count_hours, ',', 'среднее время выполнения:', timeOfHomework, 'часа')
