8e26aca8e8d9167512a24706e963684ae11d5f91
Paul <paulkuznetsov85@gmail.com>
2024-09-12 21:56:10 +0300

    5th task

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+# "Погода этой осенью - просто замечательная!"
+my_string = input('')
+print(len(my_string))
+
+my_string = "Погода этой осенью - просто замечательная!"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
