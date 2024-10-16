13db3eb648b6f0f554203ccf17ec3d702c70404a
Paul <paulkuznetsov85@gmail.com>
2024-10-16 16:56:12 +0300

    task 3_5 update

--- module_3_5.py
+++ module_3_5.py
@@ -5,8 +5,10 @@ def get_multiplied_digits(number):
 
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return first
 
-result = get_multiplied_digits(40203)
+    if first != 0:
+        return first
+    return 1
+
+result = get_multiplied_digits(420)
 print(result)
